3bad46f72600c64b5329bf44c0ea0b7e22c96242
Shilin Victor <radicaldreamer00001@gmail.com>
2019-08-01 17:06:16 +0300

    HACK: use kernel interface to read fstab
    Change-Id: Ib516d77cc1e8bed305a6d294ce9b9de918d38a8c

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -442,25 +442,6 @@ std::string ReadFstabFromDt() {
     return fstab_result;
 }
 
-// Identify path to fstab file. Lookup is based on pattern fstab.<hardware>,
-// fstab.<hardware.platform> in folders /odm/etc, vendor/etc, or /.
-std::string GetFstabPath() {
-    for (const char* prop : {"hardware", "hardware.platform"}) {
-        std::string hw;
-
-        if (!fs_mgr_get_boot_config(prop, &hw)) continue;
-
-        for (const char* prefix : {"/odm/etc/fstab.", "/vendor/etc/fstab.", "/fstab."}) {
-            std::string fstab_path = prefix + hw;
-            if (access(fstab_path.c_str(), F_OK) == 0) {
-                return fstab_path;
-            }
-        }
-    }
-
-    return "";
-}
-
 bool ReadFstabFile(FILE* fstab_file, bool proc_mounts, Fstab* fstab_out) {
     ssize_t len;
     size_t alloc_len = 0;
@@ -738,7 +719,7 @@ bool ReadDefaultFstab(Fstab* fstab) {
     if (access("/system/bin/recovery", F_OK) == 0) {
         default_fstab_path = "/etc/recovery.fstab";
     } else {  // normal boot
-        default_fstab_path = GetFstabPath();
+        default_fstab_path = "/sys/module/fsmgr_helper/parameters/fstab_smdk4x12";
     }
 
     Fstab default_fstab;
